5fce5c6752b694dea72a810ea9cca351a937401e
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2019-10-08 10:50:00 +0000

    First working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+extern crate getopts;
+
+use actix_web::{middleware, web, App, HttpServer};
+use env_logger;
+use getopts::Options;
+use std::env;
+
+mod notes;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optopt("l", "listen", "specify the address to listen on", "ADDR");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let addr = match matches.opt_str("l") {
+        Some(m) => m,
+        None => String::from("0.0.0.0:13999"),
+    };
+    let db = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    // Database Initialize
+    let client = redis::Client::open(db.as_str()).unwrap();
+
+    start(addr, client)
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} DB_STR [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn start(addr: String, client: redis::Client) {
+    HttpServer::new(move || {
+        App::new()
+            .data(client.clone())
+            .wrap(middleware::Logger::default())
+            .service(
+                web::scope("/notes")
+                    .route("/", web::post().to(notes::put_note))
+                    .route("/{id}", web::get().to(notes::get_note)),
+            )
+    })
+    .bind(addr)
+    .unwrap()
+    .run()
+    .unwrap();
 }
---
+++ src/notes.rs
@@ -0,0 +1,47 @@
+extern crate rand;
+use actix_web::{web, HttpResponse};
+use serde::{Deserialize, Serialize};
+
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+
+use redis::Commands;
+
+#[derive(Deserialize, Serialize)]
+pub struct Note {
+    data: String,
+}
+
+#[derive(Serialize)]
+pub struct NoteResponse {
+    id: String,
+}
+
+fn generate_id() -> String {
+    let rand_string: String = thread_rng().sample_iter(&Alphanumeric).take(30).collect();
+    rand_string
+}
+
+pub fn put_note(note: web::Json<Note>, db: web::Data<redis::Client>) -> HttpResponse {
+    let id = generate_id();
+    let mut con = match db.get_connection() {
+        Ok(v) => v,
+        Err(_) => {
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+    let _: () = con.set(&id, &note.data).unwrap();
+    HttpResponse::Ok().json(NoteResponse { id: id })
+}
+
+pub fn get_note(path: web::Path<String>, db: web::Data<redis::Client>) -> HttpResponse {
+    let mut con = match db.get_connection() {
+        Ok(v) => v,
+        Err(_) => {
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+    let data = con.get(path.clone()).unwrap();
+    let note = Note { data: data };
+    HttpResponse::Ok().json(note)
+}
